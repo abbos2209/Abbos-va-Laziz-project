d2faf60d405cc38bd3b07e6e5855979c2b70c2ba
Abbos <soliqsoliqsawka34@gmail.com>
2024-05-18 15:38:08 +0500

    commit3

---
+++ src/uz/app/hotel/Utill/Utill.java
@@ -0,0 +1,9 @@
+package uz.app.hotel.Utill;
+
+import java.util.Scanner;
+
+public class Utill {
+    public static Scanner intScanner = new Scanner(System.in);
+    public static Scanner strScanner = new Scanner(System.in);
+
+}
---
+++ src/uz/app/hotel/service_class/Auth.java
@@ -0,0 +1,17 @@
+package uz.app.hotel.service_class;
+
+import uz.app.hotel.service.AuthService;
+
+public class Auth implements AuthService {
+
+    @Override
+    public void signUp() {
+        System.out.println();
+    }
+
+    @Override
+    public void signIn() {
+        System.out.println("enter username");
+        String username =
+    }
+}
